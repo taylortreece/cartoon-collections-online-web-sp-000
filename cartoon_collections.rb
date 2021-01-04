84f92e23a8c8f43ed5d7489caf4c5e2e826bbe27
Taylor Treece <tayloratreece@gmail.com>
2021-01-04 17:21:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,6 @@ while i < calls_long.length
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
