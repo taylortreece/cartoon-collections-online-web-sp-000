eb802fe13a8a31868d5f80c5f8a3d1b3eaec4b55
Taylor Treece <tayloratreece@gmail.com>
2021-01-04 16:53:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |value, index| puts "#{index + 1}. #{value}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+  collection = []
+  array.map { |n| collection << n.capitalize + "!"}
+  return collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  i = 0
+while i < call.length
+  call[i].length > 4 ? true : false
+    end
+  i += 1
 end
 
 def find_the_cheese# code an argument here
