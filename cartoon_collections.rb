122e8e0ec49893ad7da35889d2372bdbea49f8eb
Taylor Treece <tayloratreece@gmail.com>
2021-01-04 17:15:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 def long_planeteer_calls(calls_long)
   i = 0
 while i < calls_long.length
-call_length  << calls_long[i].length > 4 ? true : false
+call_length << calls_long[i].length > 4 ? true : false
   i += 1
     end
     return call_length
